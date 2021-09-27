0576777c29da0c8f1e951f8a6b90b36469d0dee8
Ubuntu <ubuntu@ip-172-31-12-89.eu-central-1.compute.internal>
2021-09-27 00:40:59 +0000

    logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=make-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
-
+kubectl run prediction-app --image=minaeshak/prediction-app --port=80
+sleep 5
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/prediction-app 8000:80
 
